de4868095fc2f6e2c681c1f7cee5b5f3225e2bcd
John Quinn <jmquinn2312@gmail.com>
2022-11-28 16:03:58 -0700

    bounding box percentages obtained, had to use celebrity face model

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -8,6 +8,7 @@ import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 import Clarifai from 'clarifai';
 
+
 const app = new Clarifai.App({
   apiKey: 'ab955313c8c841e8b9b8fb729a14c098'
 });
@@ -15,48 +16,60 @@ const app = new Clarifai.App({
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-      imageUrl: ''
+    constructor() {
+      super();
+      this.state = {
+        input: '',
+        imageUrl: ''
+      }
     }
+
+  onInputChange = (event) => {
+   this.setState({input: event.target.value});
   }
 
-onInputChange = (event) => {
- this.setState({input: event.target.value})
-}
-
-  onButtonSubmit = () => {
-    console.log('click');
-    this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
-      .then(
-      function (response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function (err) {
-      });
-  }
+    onButtonSubmit = () => {
+      console.log('click');
+      this.setState({imageUrl: this.state.input })
+      app.models
+        // .predict('53e1df302c079b3db8a0a36033ed2d15', 
+      .predict('e466caa0619f444ab97497640cefc4dc', 
+          this.state.input)
+        .then(
+            function (response) {
+              console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+            },        
+            function (err) {
+              console.log(err.message);
+            }
+          );
 
 
-  render () {
-      return (
-        <div className="App">
-          <ParticlesBg type="circle" bg={true} />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm 
-            onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}
-          />
-          <FaceRecognition imageUrl={this.state.input}/>
-        </div>
-   );
-  }
+        // .then(output => console.log(output.outputs[0].data.regions[0].region_info.bounding_box))
+
+        // function (response) {
+        //   console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        // },
+        // function (err) {
+        // });
+    }
+
+
+    render () {
+        return (
+          <div className="App">
+            <ParticlesBg type="circle" bg={true} />
+            <Navigation />
+            <Logo />
+            <Rank />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={this.state.input}/>
+          </div>
+     );
+    }
 
 }
 
