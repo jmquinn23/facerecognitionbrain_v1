7b86b8e2209b9ae8d3e616a5f9f942eebd99d54f
John Quinn <jmquinn2312@gmail.com>
2022-11-28 10:44:24 -0700

    Start adding image recognition API

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,24 +1,45 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
-import 'tachyons';
 
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="circle" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+
+onInputChange = (event) => {
+ console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log("click");
+}
+
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+  render () {
+      return (
+        <div className="App">
+          <ParticlesBg type="circle" bg={true} />
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          {/*<FaceRecognition />*/}
+        </div>
+   );
+  }
+
 }
 
 export default App;
