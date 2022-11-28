df51fe4544ec72b8cfb196823db26e8634b1c21e
John Quinn <jmquinn2312@gmail.com>
2022-11-28 13:58:51 -0700

    working on facial recognition

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,29 +1,47 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'ab955313c8c841e8b9b8fb729a14c098'
+});
 
 
 class App extends Component {
 
-
-onInputChange = (event) => {
- console.log(event.target.value);
-}
-
-onButtonSubmit = () => {
-  console.log("click");
-}
-
   constructor() {
     super();
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: ''
     }
   }
+
+onInputChange = (event) => {
+ this.setState({input: event.target.value})
+}
+
+  onButtonSubmit = () => {
+    console.log('click');
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+      function (response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function (err) {
+      });
+  }
+
+
   render () {
       return (
         <div className="App">
@@ -35,7 +53,7 @@ onButtonSubmit = () => {
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
           />
-          {/*<FaceRecognition />*/}
+          <FaceRecognition imageUrl={this.state.input}/>
         </div>
    );
   }
